ed93af8140aaacadc299061891ee9e632d67e6e7
shiara <shiara.arauzo@gmail.com>
2026-01-13 00:45:40 -0500

    update sections of press

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,10 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react()],
-
-  markdown: {
-      shikiConfig: {
-          themes: {
-              light: 'github-light',
-              dark: 'github-dark',
-          },
-      },
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
