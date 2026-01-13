316a22ba160eeb6788856728203d4606d445453e
shiara <shiara.arauzo@gmail.com>
2026-01-12 23:51:57 -0500

    add new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,25 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			themes: {
-				light: 'github-light',
-				dark: 'github-dark',
-			},
-		},
-	},
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+
+  markdown: {
+      shikiConfig: {
+          themes: {
+              light: 'github-light',
+              dark: 'github-dark',
+          },
+      },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
